4dc0cfc0de44cdce64022eaecac0c53e67f1c185
Xavier Flix <xavier@xfx.net>
2018-08-07 17:53:01 -0400

    Added support to render lines with alpha degradation based n each vertex Z value

--- Tesseract/FormMain.cs
+++ Tesseract/FormMain.cs
@@ -12,11 +12,13 @@ using System.Windows.Forms;
 
 namespace Tesseract {
     public partial class FormMain : Form {
-        Matrix[] vertices;
+        private Matrix[] vertices;
         private Matrix rotationM;
-        double angle = 0;
-        const double ToRad = Math.PI / 180.0;
-        List<Tuple<int, int>> linesIndexes = new List<Tuple<int, int>>();
+        private double angle = 0;
+        private const double ToRad = Math.PI / 180.0;
+        private List<Tuple<int, int>> linesIndexes = new List<Tuple<int, int>>();
+        private bool renderWithAlpha = true;
+
 
         public FormMain() {
             InitializeComponent();
@@ -69,6 +71,7 @@ namespace Tesseract {
             //        }
             //    );
 
+            // This will only work for squares, cubes, hypercubes, etc...
             for(int i = 0; i < vertices.Length; i++) {
                 for(int j = i + 1; j < vertices.Length; j++) {
                     if(Distance(vertices[i], vertices[j]) == 2) {
@@ -92,14 +95,22 @@ namespace Tesseract {
             Graphics g = e.Graphics;
             Matrix p;
             PointF[] pts = new PointF[vertices.Length];
+            double[] zs = new double[vertices.Length];
             int pi = -1;
+            bool is4D = vertices[0].Rows == 4;
 
             g.Clear(Color.Black);
             g.TranslateTransform(this.DisplayRectangle.Width / 2, this.DisplayRectangle.Height / 2);
 
             foreach(Matrix v in vertices) {
-                p = v * rotationM.Rotate(0.6 * angle * ToRad, 0)  // Rotate X axis
-                      * rotationM.Rotate(1.0 * angle * ToRad, 3); // Rotate W axis
+                if(is4D) {
+                    p = v * rotationM.Rotate(0.6 * angle * ToRad, 0)  // Rotate X axis
+                          * rotationM.Rotate(1.0 * angle * ToRad, 3); // Rotate W axis
+                } else {
+                    p = v * rotationM.Rotate(0.6 * angle * ToRad, 0)  // Rotate X axis
+                          * rotationM.Rotate(1.0 * angle * ToRad, 1); // Rotate Y axis
+                    p *= 0.7;
+                }
 
                 // Project down from v.Rows dimensions to 2 dimensions
                 for(int di = 0; di < v.Rows - 2; di++) {
@@ -109,12 +120,49 @@ namespace Tesseract {
                 p *= 1000;
 
                 pts[++pi] = new PointF((float)p.Data[0][0], (float)p.Data[1][0]);
-                g.FillEllipse(Brushes.White, pts[pi].X - 4, pts[pi].Y - 4, 8, 8);
+                zs[pi] = p.Data[2][0];
+                g.FillEllipse(Brushes.White, pts[pi].X - 1, pts[pi].Y - 1, 3, 3);
             }
 
-            linesIndexes.ForEach((t) => g.DrawLine(Pens.White, pts[t.Item1], pts[t.Item2]));
+            if(renderWithAlpha) {
+                double minZ = zs.Min();
+                double maxZ = zs.Max();
+                linesIndexes.ForEach((t) => DrawLine(g, pts[t.Item1], pts[t.Item2], zs[t.Item1], zs[t.Item2], minZ, maxZ, 5));
+            } else {
+                linesIndexes.ForEach((t) => g.DrawLine(Pens.White, pts[t.Item1], pts[t.Item2]));
+            }
 
             angle += 1.0;
+        }
+
+        private void DrawLine(Graphics g, PointF p1, PointF p2, double z1, double z2, double minZ, double maxZ, double s = 1.0) {
+            double dx = p2.X - p1.X;
+            double dy = p2.Y - p1.Y;
+            double a = Math.Atan2(dy, dx);
+            double ca = Math.Cos(a);
+            double sa = Math.Sin(a);
+            double r = Math.Sqrt(dx * dx + dy * dy);
+            double x = p1.X;
+            double y = p1.Y;
+            double j;
+
+            for(double k = s; k < r; k += s) {
+                p2.X = (float)(x + k * ca);
+                p2.Y = (float)(y + k * sa);
+
+                j = k / r;
+                j = (1 - j) * z1 + j * z2;
+                a = Map(j, minZ, maxZ, 45.0, 255.0);
+
+                using(Pen zp = new Pen(Color.FromArgb((int)a, Color.White), 1.8f)) {
+                    g.DrawLine(zp, p1, p2);
+                };
+                p1 = p2;
+            }
+        }
+
+        private double Map(double v, double rmin, double rmax, double min, double max) {
+            return min + (v - rmin) / (rmax - rmin) * (max - min);
         }
 
         private double Distance(Matrix v1, Matrix v2) {
